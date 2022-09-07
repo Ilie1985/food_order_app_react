9652e8d8152bcd9262a8a00f6f72dfff177ccf67
Ilie1985 <ilie.marian32@yahoo.com>
2022-09-07 10:41:47 +0100

    mealItemForm input

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,10 +1,13 @@
-import React from 'react'
-im
+import React from "react";
+import styles from "./Input.module.css";
 
-function Input() {
+const Input = (props) => {
   return (
-    <div>Input</div>
-  )
-}
+    <div className={styles.input}>
+      <label htmlFor={props.input.id}>{props.label} </label>
+      <input {...props.input} />
+    </div>
+  );
+};
 
-export default Input
+export default Input;
